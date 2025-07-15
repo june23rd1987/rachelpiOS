642bf40a4c5f3bb8dbb6990c05af6b6688e9e1d5
june23rd1987 <june23rd1987@gmail.com>
2025-07-16 02:27:36 +0800

    Update startkiwix.sh

--- startkiwix.sh
+++ startkiwix.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#-------------------------------------------
+# This script is used to refresh the kiwix library upon restart to
+# include everything in the rachel modules directory. It is used
+# as part of rachelStartup.sh and various scripts in contentshell
+#
+# Authors: Sam Kinch <sam@hackersforcharity.org>     (bash version)
+#          Jonathan Field <jfield@worldpossible.org> (original perl version)
+#                                                    (& bash version tweaks)
+#          James Kainer <james@worldpossible.org>    (Updates for 3.1.2)
+# Date: 2021-07-07
+#-------------------------------------------
 
+# Restart Kiwix
+killall /usr/bin/kiwix-serve
+/usr/bin/kiwix-serve --daemon --port=81 --library /var/www/scripts/library.xml > /dev/null
